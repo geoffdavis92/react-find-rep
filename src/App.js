16d4d60ed900ced6f82467ff45904499a3b85bf8
Geoff Davis <gdavis@archermalmo.com>
2017-02-15 12:50:15 -0600

    add sample text/data to build handleAPIResponse method

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from './Form.js';
+import { endpoints, sampleResponseText } from './data'
+
+const { repLinks } = endpoints
 
 class App extends Component {
   constructor() {
     super()
-    this.state = { results: {}, resultsText: '' }
+    this.state = { results: {}, resultsText: sampleResponseText }
     this.handleAPICall = this.handleAPICall.bind(this)
   }
   handleAPICall(data, jsonString) {
-    console.log(data, jsonString)
+    console.log(data, jsonString) 
+
+    data.forEach( (rep,i,repList) => {
+      const { 
+              title,
+              first_name,
+              last_name,
+              gender,
+              birthday,
+              state,
+              state_name,
+              district,
+              chamber,
+              party,
+              website,
+              contact_form,
+              oc_email,
+              govtrack_id,
+              facebook_id,
+              twitter_id,
+              youtube_id
+            } = rep;
+      console.log({title, first_name, last_name})
+    })
+
     this.setState({ results: data, resultsText: jsonString })
   }
   render() {
@@ -20,7 +47,7 @@ class App extends Component {
         </div>
         <div className="App-body">
           <Form onAPICall={this.handleAPICall}/>
-          <pre>{this.state.resultsText}</pre>
+          <pre style={{fontSize: '12px'}}>{this.state.resultsText}</pre>
         </div>
       </div>
     );
